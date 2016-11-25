80d7ca40c5b22153d99c5fa08bc76cefdf058203
knaldhat <knaldhat@gmail.com>
2016-11-25 14:54:39 +0100

    added rarefy and refseq function to amp_load

--- R/amp_export.R
+++ R/amp_export.R
@@ -4,7 +4,7 @@
 #'
 #' @usage amp_export(data)
 #'
-#' @param data (required) A phyloseq object.
+#' @param data (required) A data list loaded with amp_load.
 #' @param file Name of the file containing the exported sequences.
 #' @param tax Add taxonomic strings to the output (default: T).
 #' 
@@ -14,7 +14,7 @@
 
 amp_export <- function(data, file = "exported_sequences.fa", tax = T){
   
-  t <- refseq(data) ######## fasta files not part of amp_load ######## UPDATE ########
+  t <- data[["refseq"]]
   
   if (tax == T){
     tax <- as.data.frame(data[["tax"]][,1:7])
--- R/amp_load.R
+++ R/amp_load.R
@@ -6,17 +6,19 @@
 #'
 #' @param otutable (required) A OTU table generated from workflow scripts v.4+.
 #' @param metadata (required) A metadata file with sample names in first column.
-#' @param refseq Reference sequences for all OTUs.
+#' @param refseq Reference sequences for all OTUs. Must be loaded with readDNAStringSet() from the biostrings package.
 #' @param rarefy Rarefy all samples to the same sequencing depth.
+#' @param percent Transform abundances from raw counts to percentages.
 #' 
 #' @return A phyloseq object.
 #' 
 #' @export
 #' @import dplyr
+#' @import vegan
 #' 
 #' @author Mads Albertsen \email{MadsAlbertsen85@@gmail.com}
 
-amp_load <- function(otutable, metadata, rarefy = NULL, percent = FALSE){
+amp_load <- function(otutable, metadata, refseq = NULL, rarefy = NULL, percent = FALSE){
   # Remove whitespace from the otutable as this will break the structure of the taxonomy
   trim <- function (x) gsub("^\\s+|\\s+$", "", x)
   otutable$Kingdom<-trim(as.character(otutable$Kingdom))
@@ -35,7 +37,13 @@ amp_load <- function(otutable, metadata, rarefy = NULL, percent = FALSE){
   #abund: all columns from otutable except the last 7 to numeric and order rows by rownames:
   abund <- as.data.frame(otutable[,1:(ncol(otutable) - 7)])/1
   abund <- abund[order(rownames(abund)),order(colnames(abund))]
-  #abundances to percent
+  
+  #rarefy function
+  if(!is.null(rarefy)){
+    abund <- rarefy(abund, sample = rarefy)
+  }
+  
+  #abundances to percent, must be done AFTER rarefy
   if(percent == TRUE) {
     abund <- as.data.frame(sapply(abund, function(x) x/sum(x)*100))
   }
@@ -45,11 +53,14 @@ amp_load <- function(otutable, metadata, rarefy = NULL, percent = FALSE){
                     ,OTU = rownames(otutable))
   tax <- tax[order(rownames(tax)), c("Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species", "OTU")]
   
-  #data: return the data in a combined list
-  data <- list(abund = abund, tax = tax, metadata = metadata)
-  
-  #rarefy function
-  #  if(!is.null(rarefy)){data <- rarefy_even_depth(data, sample.size = rarefy, rngseed = 712)}metadata <- metadata[order(rownames(metadata)), ]
+  #data: return the data in a combined list w or w/o refseq. Load
+  if(!is.null(refseq) & class(refseq) == "DNAStringSet") {
+    data <- list(abund = abund, tax = tax, metadata = metadata, refseq = refseq)
+  } else if(!is.null(refseq) & !class(refseq) == "DNAStringSet") {
+    stop("The provided reference sequences must be loaded with readDNAStringSet() from the biostrings package.")
+  } else if(is.null(refseq)) {
+    data <- list(abund = abund, tax = tax, metadata = metadata)
+  }
   
   #check if metadata and otutable match
   if(!all(rownames(data$metadata) %in% colnames(data$abund))) {
